fd396cfd05894a5c1498d7cd267188d808cba631
Paul Wooten <Paul.Wooten@hyland.com>
2021-06-21 21:04:09 -0400

    game board table

--- ng-battleship/src/app/components/game-board/game-board.component.ts
+++ ng-battleship/src/app/components/game-board/game-board.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { GameStateService } from 'src/app/services/game-state-service';
+import { GameBoard } from 'src/app/classes/game-board';
 @Component({
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
@@ -14,6 +15,9 @@ export class GameBoardComponent implements OnInit {
   private playerLabelBehaviorSubject: BehaviorSubject<string>;
   playerLabel$: Observable<string>;
 
+  private gameBoardBehaviorSubject: BehaviorSubject<GameBoard>;
+  gameBoard$: Observable<GameBoard>;
+
   constructor(private gameStateService: GameStateService) {
     this.playerNameBehaviorSubject = new BehaviorSubject<string>('');
     this.playerName$ = this.playerNameBehaviorSubject.asObservable();
@@ -22,6 +26,10 @@ export class GameBoardComponent implements OnInit {
     this.playerLabelBehaviorSubject = new BehaviorSubject<string>('');
     this.playerLabel$ = this.playerLabelBehaviorSubject.asObservable();
     this.playerLabelBehaviorSubject.next(this.gameStateService.playerLabel);
+
+    this.gameBoardBehaviorSubject = new BehaviorSubject<GameBoard>(null);
+    this.gameBoard$ = this.gameBoardBehaviorSubject.asObservable();
+    this.gameBoardBehaviorSubject.next(new GameBoard());
   }
 
   ngOnInit() {
