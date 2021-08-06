ddd783699016cc32172ca4f27725b7f3140dc121
Paul Wooten <Paul.Wooten@hyland.com>
2021-08-05 20:37:20 -0400

    user-preferences-panel tests

--- ng-battleship/src/app/components/user-preferences-panel/user-preferences-panel.component.spec.ts
+++ ng-battleship/src/app/components/user-preferences-panel/user-preferences-panel.component.spec.ts
@@ -1,16 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { UserPreferencesPanelComponent } from './user-preferences-panel.component';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatSliderModule } from '@angular/material/slider';
+
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { CellSize } from '../../classes/enumerations';
+import { UserPreferencesPanelComponent } from './user-preferences-panel.component';
+// import { UserPreferenceService } from '../../services/user-preference-service';
 
 describe('UserPreferencesPanelComponent', () => {
   let component: UserPreferencesPanelComponent;
   let fixture: ComponentFixture<UserPreferencesPanelComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UserPreferencesPanelComponent ],
-      imports: [ MatSliderModule]
+      imports: [ MatSliderModule],
+      // providers: [ { UserPreferenceService, MockUserPreferenceService } ]
     })
     .compileComponents();
   }));
@@ -24,4 +29,30 @@ describe('UserPreferencesPanelComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize cell size to medium', done => {
+    component.cellSize$.subscribe( cellSize => {
+      expect(cellSize).toEqual(CellSize.Medium);
+      done();
+    });
+  });
 });
+
+class MockUserPreferenceService {
+
+  private playerNameBehaviorSubject: BehaviorSubject<string>;
+  playerName$: Observable<string>;
+
+  private cellSizeBehaviorSubject: BehaviorSubject<CellSize>;
+  cellSize$: Observable<CellSize>;
+
+  constructor() {
+    this.cellSizeBehaviorSubject = new BehaviorSubject<CellSize>(CellSize.None);
+    this.cellSize$ = this.cellSizeBehaviorSubject.asObservable();
+    this.cellSizeBehaviorSubject.next(CellSize.Small);
+
+    this.playerNameBehaviorSubject = new BehaviorSubject<string>('');
+    this.playerName$ = this.playerNameBehaviorSubject.asObservable();
+    this.playerNameBehaviorSubject.next('Caroline');
+  }
+}
